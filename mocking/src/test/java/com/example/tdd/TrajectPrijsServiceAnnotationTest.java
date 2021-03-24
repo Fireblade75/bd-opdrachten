136906e274a30720ca0d10246925b374d20650f8
Arnold Averink <Fireblade75@users.noreply.github.com>
2021-03-24 12:12:52 +0100

    fixed annotation mock tests

--- mocking/src/test/java/com/example/tdd/TrajectPrijsServiceAnnotationTest.java
+++ mocking/src/test/java/com/example/tdd/TrajectPrijsServiceAnnotationTest.java
@@ -1,15 +1,18 @@
 package com.example.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.Mockito.*;
 
-public class TrajectPrijsServiceAnnotationTest {
+@ExtendWith(MockitoExtension.class)
+    public class TrajectPrijsServiceAnnotationTest {
 
     @Mock
     TrajectNaarTrajectEenhedenService eenhedenService;
@@ -25,18 +28,16 @@ public class TrajectPrijsServiceAnnotationTest {
         when(eenhedenService.getTrajectEenheden(eq("AMSTERDAM"), eq("ENSCHEDE"))).thenReturn(5);
         when(prijsService.getPriceTrajectEenheden(eq(5))).thenReturn(18);
 
-        assertThatCode(() -> {
-            int prijs = trajectPrijsService.getTrajectPrijs("AMSTERDAM", "ENSCHEDE");
-            assertThat(prijs).isEqualTo(18);
-        }).doesNotThrowAnyException();
+        int prijs = trajectPrijsService.getTrajectPrijs("AMSTERDAM", "ENSCHEDE");
+        assertThat(prijs).isEqualTo(18);
 
-        verify(eenhedenService.getTrajectEenheden(anyString(), anyString()));
-        verify(prijsService.getPriceTrajectEenheden(any()));
+        verify(eenhedenService).getTrajectEenheden(anyString(), anyString());
+        verify(prijsService).getPriceTrajectEenheden(anyInt());
     }
 
     @Test
     void testGetTrajectPrijsNonExisting() throws InvalidLocationException, InvalidTrajectException {
-        Mockito.when(eenhedenService.getTrajectEenheden(eq("PARIJS"), Mockito.anyString())).thenThrow(new InvalidLocationException());
+        when(eenhedenService.getTrajectEenheden(eq("PARIJS"), Mockito.anyString())).thenThrow(new InvalidLocationException());
 
         assertThatCode(() -> {
             int prijs = trajectPrijsService.getTrajectPrijs("PARIJS", "ENSCHEDE");
--- mocking/src/test/java/com/example/tdd/TrajectPrijsServiceTest.java
+++ mocking/src/test/java/com/example/tdd/TrajectPrijsServiceTest.java
@@ -5,23 +5,28 @@ import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
 
 class TrajectPrijsServiceTest {
 
     @Test
     void testGetTrajectPrijs() throws InvalidLocationException, InvalidTrajectException {
-        var trajectNaarTrajectEenhedenService = Mockito.mock(TrajectNaarTrajectEenhedenService.class);
-        var trajectEenhedenNaarPrijsService = Mockito.mock(TrajectEenhedenNaarPrijsService.class);
+        var eenhedenService = Mockito.mock(TrajectNaarTrajectEenhedenService.class);
+        var prijsService = Mockito.mock(TrajectEenhedenNaarPrijsService.class);
 
-        Mockito.when(trajectNaarTrajectEenhedenService.getTrajectEenheden(eq("AMSTERDAM"), eq("ENSCHEDE"))).thenReturn(5);
-        Mockito.when(trajectEenhedenNaarPrijsService.getPriceTrajectEenheden(eq(5))).thenReturn(18);
+        Mockito.when(eenhedenService.getTrajectEenheden(eq("AMSTERDAM"), eq("ENSCHEDE"))).thenReturn(5);
+        Mockito.when(prijsService.getPriceTrajectEenheden(eq(5))).thenReturn(18);
 
-        TrajectPrijsService trajectPrijsService = new TrajectPrijsService(trajectEenhedenNaarPrijsService, trajectNaarTrajectEenhedenService);
-        assertThatCode(() -> {
-            int prijs = trajectPrijsService.getTrajectPrijs("AMSTERDAM", "ENSCHEDE");
-            assertThat(prijs).isEqualTo(18);
-        }).doesNotThrowAnyException();
+        TrajectPrijsService trajectPrijsService = new TrajectPrijsService(prijsService, eenhedenService);
+
+        int prijs = trajectPrijsService.getTrajectPrijs("AMSTERDAM", "ENSCHEDE");
+        assertThat(prijs).isEqualTo(18);
+
+        verify(eenhedenService).getTrajectEenheden(anyString(), anyString());
+        verify(prijsService).getPriceTrajectEenheden(anyInt());
     }
 
     @Test
